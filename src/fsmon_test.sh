ba3761a8381d68a5a3ac53a67a4fe34767c82ba6
Nachiketa Prachanda <nprachan@ciena.com>
2021-11-11 10:45:48 -0800

    tests: tests config file with filesystem section
    VRVDR-56462

--- src/fsmon_test.sh
+++ src/fsmon_test.sh
@@ -4,10 +4,26 @@ daemon="${1}"
 
 PATH=$(pwd)/src:$PATH
 
-"$daemon" test.conf
+"$daemon" $(pwd)/../src/test.conf &
 pid=$!
 
+
 sleep 2
+kill -15 %1
+wait $!
+
+cat <<!EOF > test2.conf
+default_priority = "warning"
+default_facility = "local7"
+interval = 2
+!EOF
+
+"$daemon" test2.conf &
+pid=$!
+sleep 2
+
+rm -f test2.conf
+
 kill -15 %1
 wait $!
 
